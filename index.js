253e6088a5ba7b98a6cc79691f67b81126ce9f29
Hrushikesh41 <hrushikesh.kok@gmail.com>
2022-11-14 19:04:07 +0530

    Creating a Reducer Function

--- index.js
+++ index.js
@@ -8,3 +8,22 @@ function buyCake (){
         info:"First Redux Action"
     }
 }
+
+// Reducer = (previous state, action) ==> new State
+
+const initialState = {
+    numberOfCAke : 10
+}
+
+// creating a reducer function
+
+const reducer = (state = initialState, action)=>{
+    switch(action.type){
+        case BUY_CAKE : return {
+            ...state,
+            numberOfCAke : state.numberOfCAke - 1
+        }
+
+        default : return state
+    }
+}
